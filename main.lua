b92a84106cdd49c27cfa89349af0bbf05b99e40b
sdace <octagon347@gmail.com>
2017-02-10 22:52:07 -0800

    Create drawing prototype mechanic

--- main.lua
+++ main.lua
@@ -1,14 +1,30 @@
 function love.load()
-   love.graphics.setBackgroundColor(122,195,255)
+   love.graphics.setBackgroundColor(255,255,255)
+   love.graphics.setPointSize( 5 )
+   drawing = {}
+   lastMouseX = 0
+   lastMouseY = 0
 end
 
 function love.update(dt)
    -- exit game
    if love.keyboard.isDown('escape') then
-	love.event.push('quit')
-   end
+		love.event.push('quit')
+	end
+	mouseX = love.mouse.getX()
+	mouseY = love.mouse.getY()
+	if (love.mouse.isDown(1)) then
+		table.insert(drawing, {x = mouseX, y = mouseY, lastX = lastMouseX, lastY = lastMouseY})
+	end
+	lastMouseX = mouseX
+	lastMouseY = mouseY
 end
 
 function love.draw()
 	love.graphics.print("Hello World", 400, 300)
+	
+	love.graphics.setColor(0, 0, 0, 255)
+	for i,v in ipairs(drawing) do
+   		love.graphics.line(v.x, v.y, v.lastX, v.lastY)
+   	end
 end
