25faef222973ab120755cde8a99671dfa154484d
Alison <alison.wu.0606@gmail.com>
2017-02-10 21:10:07 -0800

    Add config file

--- main.lua
+++ main.lua
@@ -1,3 +1,14 @@
+function love.load()
+   love.graphics.setBackgroundColor(122,195,255)
+end
+
+function love.update(dt)
+   -- exit game
+   if love.keyboard.isDown('escape') then
+	love.event.push('quit')
+   end
+end
+
 function love.draw()
 	love.graphics.print("Hello World", 400, 300)
 end
