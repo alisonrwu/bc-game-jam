b65542eb81f654b94208f489672d4adac37f2884
Alison <alison.wu.0606@gmail.com>
2017-02-11 09:54:57 -0800

    Add a score manager

---
+++ ScoreManager.lua
@@ -0,0 +1,23 @@
+local ScoreManager = {}
+local square = {}
+local minX, maxX, minY, maxY
+local box = {}
+
+local function squareScoring(drawing)
+	print('how is my square')
+	print(drawing)
+	-- box = ScoreManager.getBox(drawing)
+	for i = 1, #drawing do
+		
+	end
+end
+
+local function getBox(drawing)
+	for i,v in ipairs(drawing) do
+
+	end
+end
+
+ScoreManager.squareScoring = squareScoring
+
+return ScoreManager
