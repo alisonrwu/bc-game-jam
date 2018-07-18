026a6dbd66500df1132bb9c0a4106d5c29942585
terbb <trevinwong@gmail.com>
2018-07-18 02:07:00 -0700

    Add oval as a new problem.

---
+++ src/states/game/problems/Oval.lua
@@ -0,0 +1,53 @@
+Oval = Shape:extend("Oval")
+
+function Oval:init(widthInGameUnits, heightInGameUnits, maxScore)
+  Oval.super:init(widthInGameUnits, heightInGameUnits, maxScore)
+  self.xRad, self.yRad = self.dimensions.width * 0.5, self.dimensions.height * 0.5
+  self.left, self.top, self.right, self.bot = Point(), Point(), Point(), Point()
+  self.centre = Point()
+end
+
+function Oval:draw()
+  if self.displayAnswer then Graphics:drawLineEllipse(self.centre.x, self.centre.y, self.xRad, self.yRad, Graphics.GREEN) end
+end
+
+function Oval:score(drawing)
+  self:points(drawing.centre)
+  return Oval.super.score(self, drawing)
+end
+
+function Oval:points(centre)
+  self.left = {x = centre.x - self.xRad, y = centre.y}
+  self.top = {x = centre.x, y = centre.y - self.yRad}
+  self.right = {x = centre.x + self.xRad, y = centre.y}
+  self.bot = {x = centre.x, y = centre.y + self.yRad}  
+  self.centre = centre
+end
+
+function Oval:pointRepresentation()
+  local pointOval = {}
+  local numOfSteps = 10
+  local width, height = self.dimensions.width, self.dimensions.height
+  local widthStep = width / numOfSteps
+  
+  local function ellipseFunctionY(x)
+    return height * math.sqrt(0.25 - (x/width) * (x/width))
+  end
+  
+  local halfW = width * 0.5
+  local halfH = height * 0.5
+  
+  for i = 0, width, widthStep do
+    pointOval[#pointOval + 1] = {x = self.left.x + i, y = self.top.y + halfH - ellipseFunctionY(i - halfW)}
+  end
+  
+  for i = 0, width, widthStep do
+    pointOval[#pointOval + 1] = {x = self.right.x - i, y = self.top.y + halfH + ellipseFunctionY(i - halfW)} 
+  end
+
+  return pointOval
+end
+
+function Oval:area()
+  return self.xRad * self.yRad * math.pi
+end
