96c32c3c20f8be65aab391e47aff2ba05de5df91
terbb <trevinwong@gmail.com>
2018-07-21 23:30:53 -0700

    Make instructions clearer

--- src/states/Instructions.lua
+++ src/states/Instructions.lua
@@ -1,38 +1,64 @@
-Instructions = State:extend("Instructions", {CYCLE = 15})
+Instructions = State:extend("Instructions")
 
 function Instructions:init()
   self.menuBG = ImagePlaceable("assets/graphics/menu/bg/bg_menu16x9.png")
-  
-  self.instructions_1 = TextPlaceable("The boss wants you to cut some paper...", "center")
-  self.instructions_1:setCentreHorizontal(self.menuBG)
-  self.instructions_1.position.y = baseRes.height * 0.1
-  
-  self.instructions_2 = TextPlaceable("Better do what he says, fast!", nil, "center", Graphics.RED)
-  self.instructions_2:setBelow(self.instructions_1, 50)
-  
-  self.instructions_3 = TextPlaceable("Use the left mouse button to cut the proper size sheets of paper.", nil, "center")
-  self.instructions_3:setBelow(self.instructions_2, 50)
-  
-  self.clickToStart = FlashingTextPlaceable("Click to Start!", nil, "center")
-  self.clickToStart:setBelow(self.instructions_3, 100)
+  self.placeables = {}
+  self.onMouseRelease = function() end
+  self:setUpScreen1()
 end
 
 function Instructions:update(dt)
-  self.clickToStart:update()
+  for _, v in ipairs(self.placeables) do
+    v:update()
+  end
 end                            
   
 function Instructions:draw()  
   self.menuBG:draw()
-  self.instructions_1:draw()
-  self.instructions_2:draw()
-  self.instructions_3:draw()
-  self.clickToStart:draw()
+  for _, v in ipairs(self.placeables) do
+    v:draw()
+  end
+end
+
+function Instructions:setUpScreen1()
+  local i1 = TextPlaceable("The boss wants you to cut some paper...", nil, "center", Graphics.NORMAL, 1)
+  i1.position.y = baseRes.height * 0.09
+  
+  local speechBubble = ImagePlaceable("assets/graphics/game/hud/hud_speechbubble.png")
+  speechBubble:setCentreHorizontal(self.menuBG)
+  speechBubble:setBelow(i1, 20)
+  
+  local example = TextPlaceable("I want a 3W x 2L Rectangle!")
+  example:setCentreHorizontal(self.menuBG)
+  example:setCentreVertical(speechBubble)
+  
+  local i2 = TextPlaceable("W = width   L = length", nil, "center")
+  i2:setBelow(example, 20)
+  
+  local i3 = TextPlaceable("The closer you are, the more points you get!", nil, "center")
+  i3:setBelow(i2, 45)
+
+  local i4 = TextPlaceable("Collect points to gain more time!", nil, "center")
+  i4:setBelow(i3, 10)
+  
+  local i5 = TextPlaceable("If you run out of time, you're fired!", nil, "center", Graphics.RED)
+  i5:setBelow(i4, 10)
+  
+  local i6 = FlashingTextPlaceable("Click to start!", nil, "center")
+  i6:setBelow(i5, 50)
+  
+  local mouseRelease = function()
+    Sound:createAndPlay("assets/audio/sfx/sfx_click.mp3", "click")
+    state = Game()
+  end
+  
+  self.placeables = {i1, i2, speechBubble, example, i3, i4, i5, i6}
+  self.onMouseRelease = mouseRelease
 end
 
 function Instructions:mousePressed(x, y, button, isTouch)
 end
 
 function Instructions:mouseRelease(x, y, button, istouch)
-  Sound:createAndPlay("assets/audio/sfx/sfx_click.mp3", "click")
-  state = Game()
+  self:onMouseRelease()
 end    
